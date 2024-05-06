f611e6000d1923578b6527aee57d7fa2af871b94
l0cas44 <lucas.moura.paixao@gmail.com>
2024-05-06 18:53:49 -0300

    Add files via upload
    atividade em sala infinity

---
+++ atividade js/index.js
@@ -0,0 +1,14 @@
+let lista = [
+  'maça',
+  'ovelha',
+  44,
+  'abelha',
+ {
+  name:'lucas',
+  age: 33,
+  isAdm: true
+ },
+ true 
+]
+console.log(`A segunda linha do Arrey é ${lista[4].name}`)
+
